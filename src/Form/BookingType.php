9652da81c0950f310e94cdc263d6c2cda4e7404b
vladimir.lasac <vladimir.lasac@connectagroup.eu>
2025-09-30 09:55:06 +0200

    booking crud created and routes are protected with user roles

---
+++ src/Form/BookingType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Booking;
+use App\Entity\Room;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('start_date')
+            ->add('end_date')
+            ->add('total_price')
+            ->add('customer', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+            ->add('room', EntityType::class, [
+                'class' => Room::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Booking::class,
+        ]);
+    }
+}
