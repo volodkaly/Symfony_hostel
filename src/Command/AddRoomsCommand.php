3cc88c3c33a757781e35dae4df78cc48d1d9b988
vladimir.lasac <vladimir.lasac@connectagroup.eu>
2025-11-26 12:25:04 +0100

    mock data database filling is improved with a command. We need to pass EntityManagerInterface into contruct as PRIVATE! very important, otherwise just does not work.

---
+++ src/Command/AddRoomsCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Room;
+use App\Repository\RoomRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'addRooms',
+    description: 'Add a short description for your command',
+)]
+class AddRoomsCommand extends Command
+{
+    public function __construct(private EntityManagerInterface $em)
+    {
+        parent::__construct();
+    }
+
+
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $room = (new Room())->setName('Room' . rand(20, 100))->setCapacity(rand(1, 8))->setPrice(rand(200, 300));
+
+        $this->em->persist($room);
+        $this->em->flush();
+
+        echo 'Room added: ' . PHP_EOL . $room->getName() . PHP_EOL . 'price: ' . $room->getPrice() . PHP_EOL . 'capacity: ' . $room->getCapacity() . PHP_EOL;
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Command/AddUserCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'addUser',
+    description: 'Add a short description for your command',
+)]
+class AddUserCommand extends Command
+{
+    public function __construct(private EntityManagerInterface $em)
+    {
+        parent::__construct();
+    }
+
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        for ($i = 0; $i < 100; $i++) {
+
+            $user = (new User())->setName('User' . rand(20, 100))->setEmail('user' . random_int(10000, 100000) . '@example.com')->setPassword(password_hash(1, PASSWORD_BCRYPT))->setRoles(['ROLE_USER']);
+
+            $this->em->persist($user);
+            $this->em->flush();
+
+            echo 'User added: ' . PHP_EOL . $user->getName() . PHP_EOL . 'email: ' . $user->getEmail() . PHP_EOL;
+        }
+
+        return Command::SUCCESS;
+    }
+}
