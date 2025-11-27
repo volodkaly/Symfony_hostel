afac7fbd42c0ffb1ca88a5a97717402d10746bde
vladimir.lasac <vladimir.lasac@connectagroup.eu>
2025-11-27 12:13:59 +0100

    average room ratings are calculating and displayed on the main page with rooms index list

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Booking;
+use App\Repository\BookingRepository;
+use App\Repository\ReviewRepository;
+use App\Repository\RoomRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,9 +18,10 @@ final class RoomController extends AbstractController
 
 
     #[Route('/rooms', name: 'app_room')]
-    public function index(EntityManagerInterface $em, Request $request): Response
+    public function index(EntityManagerInterface $em, Request $request, RoomRepository $roomRepository, BookingRepository $bookingRepository, ReviewRepository $reviewRepository): Response
     {
         $page = $request->query->getInt('page', 1);
+
         $rooms = $em->createQueryBuilder()
             ->select('r')
             ->from(Room::class, 'r')
@@ -25,12 +30,29 @@ final class RoomController extends AbstractController
             ->getQuery()
             ->getArrayResult();
 
-
+        foreach ($rooms as &$room) {
+            $roomMarks = [];
+            $bookings = $bookingRepository->findBy(['room' => $room['id']]);
+            foreach ($bookings as $booking) {
+                $foundReviews = $reviewRepository->findBy(['Booking' => $booking->getId()]);
+                foreach ($foundReviews as $review) {
+                    $roomMarks[] = $review->getMark();
+                }
+            }
+            if (count($roomMarks) > 0) {
+                $avg = array_sum($roomMarks) / count($roomMarks);
+                $room['average_rating'] = number_format($avg, 2);
+            } else {
+                $room['average_rating'] = 'no rating yet';
+            }
+        }
+        unset($room);
 
         return $this->render('room/index.html.twig', [
             'controller_name' => 'RoomController',
             'rooms' => $rooms,
             'page' => $page,
+
         ]);
     }
 }
