1e82feb21c7a612b10fa94d4a0206429cb00584a
vladimir.lasac <vladimir.lasac@connectagroup.eu>
2025-12-10 10:29:50 +0100

    date filtration added

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -2,68 +2,81 @@
 
 namespace App\Controller;
 
-use App\Entity\Booking;
-use App\Entity\Review;
-use App\Repository\BookingRepository;
-use App\Repository\ReviewRepository;
-use App\Repository\RoomRepository;
+use App\Entity\Room;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-use Doctrine\ORM\EntityManagerInterface;
-use App\Entity\Room;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class RoomController extends AbstractController
 {
-    #[Route('/rooms', name: 'app_room')] public function index(EntityManagerInterface $em, Request $request, RoomRepository $roomRepository, BookingRepository $bookingRepository, ReviewRepository $reviewRepository): Response
+    #[Route('/rooms', name: 'app_room')]
+    public function index(EntityManagerInterface $em, Request $request): Response
     {
         $page = $request->query->getInt('page', 1);
         $maxPrice = $request->query->getInt('maxPrice', 1000);
         $minRating = $request->query->getInt('minRating', 1);
         $minCapacity = $request->query->getInt('minCapacity', 1);
+        $start = $request->query->get('start');
+        $end = $request->query->get('end');
 
-        $results = $em->createQueryBuilder()
+        $qb = $em->createQueryBuilder()
             ->select('r', 'AVG(rev.mark) as average_rating')
             ->from(Room::class, 'r')
             ->where('r.price <= :maxPrice')
             ->setParameter('maxPrice', $maxPrice)
             ->andWhere('r.capacity >= :minCapacity')
             ->setParameter('minCapacity', $minCapacity)
-            ->having('average_rating >= :minRating OR average_rating IS NULL')
-            ->setParameter('minRating', $minRating)
             ->leftJoin('r.bookings', 'b')
             ->leftJoin('b.review', 'rev')
-            ->groupBy('r.id');
 
-        $results2 = clone $results;
-        $numberOfResults = count($results2->getQuery()->getArrayResult());
+            ->groupBy('r.id')
+            ->having('average_rating >= :minRating OR average_rating IS NULL')
+            ->setParameter('minRating', $minRating);
 
-        $resultsWithPagination = $results
-            ->setFirstResult(10 * $page - 10)
+        if ($start && $end) {
+            $start = new \DateTime($start);
+            $end = new \DateTime($end);
+
+            $qb->leftJoin(
+                'r.bookings',
+                'b_occupied',
+                'WITH',
+                'b_occupied.start_date < :end AND b_occupied.end_date > :start'
+            );
+            $qb->andWhere('b_occupied.id IS NULL');
+            $qb->setParameter('start', $start);
+            $qb->setParameter('end', $end);
+        }
+
+        $countQuery = clone $qb;
+        $numberOfResults = count($countQuery->getQuery()->getArrayResult());
+
+        $resultsWithPagination = $qb
+            ->setFirstResult(10 * ($page - 1))
             ->setMaxResults(10)
+            ->orderBy('r.price', 'ASC')
             ->getQuery()
             ->getArrayResult();
 
-        //array structure transformation 
         $roomsWithRatings = array_map(function ($item) {
             $room = $item[0];
-            $room['average_rating'] = $item['average_rating'] ? number_format($item['average_rating'], 2) : 'N/A';
+            $room['average_rating'] = $item['average_rating'] ? number_format((float) $item['average_rating'], 2) : 'N/A';
             return $room;
         }, $resultsWithPagination);
 
-
-        return $this->render(
-            'room/index.html.twig',
-            [
-                'controller_name' => 'RoomController',
-                'rooms' => $roomsWithRatings,
-                'page' => $page,
-                'maxPrice' => $maxPrice,
-                'minRating' => $minRating,
-                'minCapacity' => $minCapacity,
-                'numberOfResults' => $numberOfResults,
-            ]
-        );
+        return $this->render('room/index.html.twig', [
+            'controller_name' => 'RoomController',
+            'rooms' => $roomsWithRatings,
+            'page' => $page,
+            'maxPrice' => $maxPrice,
+            'minRating' => $minRating,
+            'minCapacity' => $minCapacity,
+            'numberOfResults' => $numberOfResults,
+            'start' => $start ? $start->format('Y-m-d') : (new DateTime('today'))->format('Y-m-d'),
+            'end' => $end ? $end->format('Y-m-d') : (new DateTime('today'))->format('Y-m-d')
+        ]);
     }
 }
--- src/Repository/RoomRepository.php
+++ src/Repository/RoomRepository.php
@@ -16,28 +16,5 @@ class RoomRepository extends ServiceEntityRepository
         parent::__construct($registry, Room::class);
     }
 
-    //    /**
-    //     * @return Room[] Returns an array of Room objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('r')
-    //            ->andWhere('r.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('r.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
 
-    //    public function findOneBySomeField($value): ?Room
-    //    {
-    //        return $this->createQueryBuilder('r')
-    //            ->andWhere('r.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
