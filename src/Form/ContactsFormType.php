e842f8e7d35616eaf12645011fff66fd83b7788c
vladimir.lasac <vladimir.lasac@connectagroup.eu>
2025-12-12 14:30:54 +0100

    styled

--- src/Form/ContactsFormType.php
+++ src/Form/ContactsFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Message;
 use App\Entity\User;
 use Doctrine\DBAL\Types\TextType;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,7 +16,7 @@ class ContactsFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content');
+            ->add('content', \Symfony\Component\Form\Extension\Core\Type\TextType::class, ['attr' => ['class' => 'contact-input']]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
